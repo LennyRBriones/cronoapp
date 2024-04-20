c156101855c04f929ba8de2f5955040efd59a336
LennyRBriones <lennyrbriones@outlook.com>
2024-04-20 17:56:14 -0600

    first commit

---
+++ app/src/main/java/com/lennyrbriones/cronoapp/room/CronosDatabaseDao.kt
@@ -0,0 +1,33 @@
+package com.lennyrbriones.cronoapp.room
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.lennyrbriones.cronoapp.model.Cronos
+import kotlinx.coroutines.flow.Flow
+
+
+// Interface -> Repository -> Viewmodel -> View
+
+@Dao // Data Access Observer
+interface CronosDatabaseDao {
+
+    // Crud
+    @Query("SELECT * FROM cronos")
+    fun getCronos(): Flow<List<Cronos>>
+
+    @Query("SELECT * FROM cronos WHERE id = :id")
+    fun getCronosById(id: Long): Flow<Cronos>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(crono: Cronos)
+
+    @Update(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun update(crono: Cronos)
+
+    @Delete
+    suspend fun delete(crono: Cronos)
+}
